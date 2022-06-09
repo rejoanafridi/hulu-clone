e04fdf0aa2f7afb58084d5d5e90e0b1a8b35ab1f
rejoanafridi <rejoanafridi93@gmail.com>
2022-06-10 01:14:17 +0600

    complete

--- components/Header.js/HeaderItem.js
+++ components/Header.js/HeaderItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+
 function HeaderItem({ Icon, title }) {
+
 	return (
-		<div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white mt-3">
+		<div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
 			<Icon className="h-8 mb-2 group-hover:animate-bounce" />
-			<p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
+			<p className="opacity-0 group-hover:opacity-100 tracking-widest">
+				{title}
+			</p>
 		</div>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import CurdItem from "../components/CurdItem/CurdItem";
 import Header from "../components/Header.js/Header";
 import Navbar from "../components/Nav.js/Navbar";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({results}) {
+	// console.log(results);
 	return (
 		<div className="">
 			<Head>
@@ -13,15 +16,30 @@ export default function Home() {
 			</Head>
 
 			<main>
-			
-        {/* header */}
-        <Header />
-        {/* nav */}
+				{/* header */}
+				<Header />
+				{/* nav */}
 
-        <Navbar />
-        
+				<Navbar />
 
+				{/* results */}
+				<CurdItem results={results} />
 			</main>
 		</div>
 	);
+}
+// get api data
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+	//  fetch data
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || requests.fetchTrending.url
+		}`
+	).then((res) => res.json());
+	return {
+		props: {
+			results: request.results,
+		},
+	};
 }
